574db54f089d3a8c84c5f211724ba7f51cc01534
Henri Karhu <karhu.henri@gmail.com>
2016-02-07 19:07:05 +0200

    viikon kaksi tasalle

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,20 +1,9 @@
 class Beer < ActiveRecord::Base
+include RatingAverage
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
-def average_rating
-if ratings.count != 0
-   average =0.0
-   total = 0.0  
-   ratings.map do |rating|
-   total = total + rating.score
-end
-   average = total / ratings.count
-   "#{average}"
-else
-   "0"
-end
-end
+
 
 def to_s
  "#{self.name}, #{brewery.name}"
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,15 +1,6 @@
 class Brewery < ActiveRecord::Base
-  has_many :beers, dependent: :destroy
+  include RatingAverage
 
-def print_report
-    puts name
-    puts "established at year #{year}"
-    puts "number of beers #{beers.count}"
-  end
-
-  def restart
-    self.year = 2016
-    puts "changed year to #{year}"
-  end
-
+  has_many :beers
+  has_many :ratings, through: :beers
 end
